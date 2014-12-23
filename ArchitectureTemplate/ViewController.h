bebd3f8fcfc875ee3d8aa2f1e77620646e24d02a
Jamie Kelly <james@builtforge.com>
2014-12-23 21:24:14 +0900

    Initial Commit

---
+++ ArchitectureTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArchitectureTemplate
+//
+//  Created by Jamie on 23/12/14.
+//  Copyright (c) 2014 James Kelly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
