ac87177a772649c03549ac11bf2c71e81cca6253
Jamie Kelly <james@builtforge.com>
2014-12-25 12:35:46 +0900

    Added more notes.

--- ArchitectureTemplate/ListViewModel.h
+++ ArchitectureTemplate/ListViewModel.h
@@ -22,8 +22,15 @@
 - (NSString *)titleForSecondaryItemAtIndex:(NSInteger)index;
 
 /**
- *  As stated in
- *
+ *  As stated in ViewModel.h, we do not pass out our object models directly to clients of the view model.
+ *  This is the exception - we expose our objects for the view controller client to build a view model
+ *  to pass to another view controller.  
+ *  If you don't want to do this you could have your view models build required view models for passing to other
+ *  classes instead.  This would probably done on a case-by-case basis, such as giving this ListViewModel class
+ *  a method like 
+ *  - (DetailViewModel *)detailViewModelForItemAtIndex:(NSInteger)index;
+ *  That way you would never have to expose the item class to your VCs, but your view models would then have to
+ *  know something about each other (in this case ListViewModel knows about DetailViewModel).
  */
 - (id)primaryItemAtIndex:(NSInteger)index;
 - (id)secondaryItemAtIndex:(NSInteger)index;
